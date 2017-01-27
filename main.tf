33de37014cb8f9f95f717b6783de93700bfb7399
HashiCorp Training <training@hashicorp.com>
2017-01-27 18:12:48 +0000

    commit 1

---
+++ main.tf
@@ -0,0 +1,58 @@
+#
+# DO NOT DELETE THESE LINES!
+#
+# Your subnet ID is:
+#
+#     subnet-ddd57685
+#
+# Your security group ID is:
+#
+#     sg-29ef374e
+#
+# Your AMI ID is:
+#
+#     ami-30217250
+#
+# Your Identity is:
+#
+#     autodesk-chipmunk
+#
+
+variable "aws_access_key" {
+  type = "string"
+}
+
+variable "aws_secret_key" {
+  type = "string"
+}
+
+variable "aws_region" {
+  default = "us-west-1"
+}
+
+variable "num_webs"{
+	default="2"
+}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-30217250"
+  instance_type          = "t2.micro"
+  subnet_id              = "subnet-ddd57685"
+  vpc_security_group_ids = ["sg-29ef374e"]
+	count="${var.num_webs}"
+  tags {
+    Identity = "autodesk-chipmunk"
+    Purpose  = "Terraform training"
+    Name     = "web ${count.index+1}"
+  }
+}
+
+output public_dns {
+  value = ["${aws_instance.web.*.public_dns}"]
+}
